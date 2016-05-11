68954b1ada8cd5c3492699bef886ba2d4f070c32
Michael Cavalea <callmecavs@gmail.com>
2016-05-11 18:32:34 -0400

    repo cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 const prefixer = require('autoprefixer')
-const sync     = require('browser-sync')
 const cssnano  = require('cssnano')
 const del      = require('del')
 const gulp     = require('gulp')
-const htmlmin  = require('gulp-htmlmin')
 const plumber  = require('gulp-plumber')
 const postcss  = require('gulp-postcss')
 const sass     = require('gulp-sass')
-const maps     = require('gulp-sourcemaps')
 const notifier = require('node-notifier')
 
 // error handler
@@ -26,15 +23,6 @@ const onError = function(error) {
 
 gulp.task('clean', () => del('dist'))
 
-// html
-
-gulp.task('html', () => {
-  return gulp.src('src/html/**/*.html')
-    .pipe(plumber({ errorHandler: onError }))
-    .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-    .pipe(gulp.dest('dist'))
-})
-
 // sass
 
 const processors = [
@@ -48,52 +36,19 @@ gulp.task('sass', () => {
     .pipe(maps.init())
     .pipe(sass())
     .pipe(postcss(processors))
-    .pipe(maps.write('./maps', { addComment: false }))
     .pipe(gulp.dest('dist'))
 })
-
-// server
-
-const server = sync.create()
-const reload = sync.reload
-
-const sendMaps = (req, res, next) => {
-  const filename = req.url.split('/').pop()
-  const extension = filename.split('.').pop()
-
-  if(extension === 'css' || extension === 'js') {
-    res.setHeader('X-SourceMap', '/maps/' + filename + '.map')
-  }
-
-  return next()
-}
-
-const options = {
-  notify: false,
-  server: {
-    baseDir: 'dist',
-    middleware: [
-      sendMaps
-    ]
-  },
-  watchOptions: {
-    ignored: '*.map'
-  }
-}
-
-gulp.task('server', () => sync(options))
 
 // watch
 
 gulp.task('watch', () => {
-  gulp.watch('src/html/**/*.html', ['html', reload])
   gulp.watch('src/sass/**/*.scss', ['sass', reload])
 })
 
 // build and default tasks
 
 gulp.task('build', ['clean'], () => {
-  gulp.start('html', 'sass')
+  gulp.start('sass')
 })
 
-gulp.task('default', ['build', 'server', 'watch'])
+gulp.task('default', ['build', 'watch'])
